ba46ca4f02fe0a2664769e993de0360d6ae28876
Duncan Caruthers <duncan.caruthers01@gmail.com>
2022-11-28 19:23:38 -0500

    Angular is stupid and I hate it

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/e-commerce'),
+      dir: require('path').join(__dirname, './coverage/fusion-frontend'),
       subdir: '.',
       reporters: [
         { type: 'html' },
