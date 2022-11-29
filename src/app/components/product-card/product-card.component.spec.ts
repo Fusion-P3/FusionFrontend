75e3ddc6b9ae6b10b4e465844a24897516ff103a
Gregory Sabado <gregory793@revature.net>
2022-11-29 13:54:06 -0800

    test checks for after call to product service

--- src/app/components/product-card/product-card.component.spec.ts
+++ src/app/components/product-card/product-card.component.spec.ts
@@ -1,12 +1,15 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service';
 
 import { ProductCardComponent } from './product-card.component';
 
 describe('ProductCardComponent', () => {
   let component: ProductCardComponent;
   let fixture: ComponentFixture<ProductCardComponent>;
+  let service: ProductService;
+  let testProd = new Product(1, 'test', 10, 'test', 1, 'test');
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -17,6 +20,7 @@ describe('ProductCardComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ProductCardComponent);
+    service = TestBed.inject(ProductService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -26,7 +30,14 @@ describe('ProductCardComponent', () => {
   });
 
   it('should update cart when product/quantity is added', () => {
-    component.addToCart(new Product(1, 'test', 1, 'test', 1, 'test'));
+    component.addToCart(testProd);
     expect(component.cartCount == 1);
+    component.addToCart(testProd);
+    expect(component.cartCount == 1);
+    service.getCart().subscribe((cart) => {
+      expect(cart.cartCount == 1);
+      expect(cart.products[0].quantity == 2);
+      expect(cart.products[0].product == testProd);
+    });
   });
 });
