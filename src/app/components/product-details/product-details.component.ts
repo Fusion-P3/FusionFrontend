e83c649705de78a41821f3d467dc8c67ed214f19
Duncan Caruthers <duncan.caruthers01@gmail.com>
2022-11-30 16:09:30 -0500

    Merge fix

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -2,7 +2,8 @@ import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Product } from 'src/app/models/product';
-import { ProductService } from 'src/app/services/product.service'
+import { AuthService } from 'src/app/services/auth.service';
+import { CartDto, ProductService } from 'src/app/services/product.service'
 import { WikiService } from 'src/app/services/wiki.service';
 
 @Component({
@@ -10,9 +11,9 @@ import { WikiService } from 'src/app/services/wiki.service';
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.scss']
 })
-export class ProductDetailsComponent implements OnInit{
+export class ProductDetailsComponent implements OnInit {
 
-  public product: Product = new Product(0,'TEST',0,'',0,'');
+  public product: Product = new Product(0, 'TEST', 0, '', 0, '');
   fullDescription: string = '';
   cartCount!: number;
   products: {
@@ -25,7 +26,7 @@ export class ProductDetailsComponent implements OnInit{
 
   @Input() productInfo!: Product;
 
-  constructor(private route: ActivatedRoute, private productService: ProductService, private wikiService: WikiService, private cdr: ChangeDetectorRef) { }
+  constructor(private route: ActivatedRoute, private productService: ProductService, private wikiService: WikiService, private cdr: ChangeDetectorRef, private auth: AuthService) { }
 
   ngOnInit(): void {
     let productname = this.route.snapshot.paramMap.get('productname')!
@@ -37,23 +38,26 @@ export class ProductDetailsComponent implements OnInit{
         });
       }
     });
-    this.subscription = this.productService.getCart().subscribe((cart) => {
+    this.subscription = this.productService.getCart(this.auth.userId).subscribe((cart) => {
       this.cartCount = cart.cartCount;
       this.products = cart.products;
       this.totalPrice = cart.totalPrice;
     });
   }
-  
+
   addToCart(product: Product): void {
     let inCart = false;
 
     this.products.forEach((element) => {
       if (element.product.name == product.name) {
         element.quantity += this.quantitySelect;
-        let cart = {
-          cartCount: this.cartCount,
-          products: this.products,
-          totalPrice: this.totalPrice + product.price,
+        let cart: CartDto = {
+          userId: this.auth.userId,
+          cart: {
+            cartCount: this.cartCount,
+            products: this.products,
+            totalPrice: this.totalPrice + product.price
+          }
         };
         this.productService.setCart(cart);
         inCart = true;
@@ -66,10 +70,13 @@ export class ProductDetailsComponent implements OnInit{
         quantity: this.quantitySelect,
       };
       this.products.push(newProduct);
-      let cart = {
-        cartCount: this.cartCount + 1,
-        products: this.products,
-        totalPrice: this.totalPrice + product.price,
+      let cart: CartDto = {
+        userId: this.auth.userId,
+        cart: {
+          cartCount: this.cartCount,
+          products: this.products,
+          totalPrice: this.totalPrice + product.price
+        }
       };
       this.productService.setCart(cart);
     }
