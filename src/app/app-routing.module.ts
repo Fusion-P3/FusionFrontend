5f1868a0db4c162fe23bf664cf43eb89f26a47b4
Jacob Allen <jallen1887@gmail.com>
2022-11-30 17:19:39 -0500

    Fix cart update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,8 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,
+    { enableTracing: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
