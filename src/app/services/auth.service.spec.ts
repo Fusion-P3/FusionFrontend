65f380f6163866a1fcfcf6b69c8d6f52edf8adce
Duncan Caruthers <duncan.caruthers01@gmail.com>
2022-12-01 13:02:13 -0500

    Auth tests

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,19 +1,48 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { userDTO } from '../components/login/login.component';
 
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let controller: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule]
     });
     service = TestBed.inject(AuthService);
+    controller = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+    service.logout();
+    expect(service).toBeTruthy();
+  });
+
+  it('should login', () => {
+    let dto: userDTO = {
+      username: 'test ',
+      password: 'password'
+    };
+
+    service.login(dto).subscribe((response) => {
+      expect(response).toBe(dto);
+    });
+    const res = controller.expectOne('https://localhost:7078/auth/login');
+    expect(res.request.method).toBe('POST');
+    res.flush(dto);
+  });
+
+  it('should register', () => {
+
+    service.register('test', 'test', 'test', 'test', 'test').subscribe((response) => {
+      expect(response).toBeTruthy()
+    });
+    const res = controller.expectOne('https://localhost:7078/auth/register');
+    expect(res.request.method).toBe('POST');
+    res.flush(true);
   });
 });
