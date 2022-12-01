6a05b1c7aa9ea6ba04b976e777188383ab44ff58
James Monty-Carbonari <james@machinateddesigns.com>
2022-12-01 16:55:36 -0500

    Don't mess with Auth Services

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -7,29 +7,35 @@ import { CartDto, ProductService } from 'src/app/services/product.service';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
-
   products: {
-    product: Product,
-    quantity: number
+    product: Product;
+    quantity: number;
   }[] = [];
   totalPrice!: number;
   cartProducts: Product[] = [];
 
-  constructor(private productService: ProductService, private router: Router, private auth: AuthService) { }
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+    private auth: AuthService
+  ) {}
+
+  username = this.auth.userName;
 
   ngOnInit(): void {
-    this.productService.getCart(this.auth.userId).subscribe(
-      (cart) => {
-        this.products = cart.products;
-        this.products.forEach(
-          (element) => this.cartProducts.push(element.product)
-        );
-        this.totalPrice = cart.totalPrice;
-      }
-    );
+    this.productService.getCart(this.auth.userId).subscribe((cart) => {
+      this.products = cart.products;
+      this.products.forEach((element) =>
+        this.cartProducts.push(element.product)
+      );
+      this.totalPrice = this.products.reduce(
+        (a, b) => a + b.product.price * b.quantity,
+        0
+      );
+    });
   }
 
   emptyCart(): void {
@@ -38,19 +44,40 @@ export class CartComponent implements OnInit {
       cart: {
         cartCount: 0,
         products: [],
-        totalPrice: 0.00
-      }
+        totalPrice: 0.0,
+      },
     };
-    this.productService.setCart(cart).subscribe(
-      (cart) => {
-        this.products = cart.cart.products;
-        this.products.forEach(
-          (element) => this.cartProducts.push(element.product)
-        );
-        this.totalPrice = cart.cart.totalPrice;
-      }
-    );
+    this.productService.setCart(cart).subscribe((cart) => {
+      this.products = cart.cart.products;
+      this.products.forEach((element) =>
+        this.cartProducts.push(element.product)
+      );
+      this.totalPrice = cart.cart.totalPrice;
+    });
     this.router.navigate(['/home']);
   }
 
+  removeFromCart(id: number): any {
+    this.products = this.products.filter((product) => product.product.id != id);
+
+    let cart: CartDto = {
+      userId: this.auth.userId,
+      cart: {
+        cartCount: this.products.length,
+        products: this.products,
+        totalPrice: this.products.reduce(
+          (a, b) => a + b.product.price * b.quantity,
+          0
+        ),
+      },
+    };
+
+    this.productService.setCart(cart).subscribe((cart) => {
+      this.products = cart.cart.products;
+      this.products.forEach((element) =>
+        this.cartProducts.push(element.product)
+      );
+      this.totalPrice = cart.cart.totalPrice;
+    });
+  }
 }
