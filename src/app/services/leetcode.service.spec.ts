6497d5824459ced5a6f4b57baef4cae46baeef8d
Gregory Sabado <gregory793@revature.net>
2022-12-01 09:44:29 -0800

    fixed leetcode service test

--- src/app/services/leetcode.service.spec.ts
+++ src/app/services/leetcode.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { LeetcodeService } from './leetcode.service';
@@ -6,7 +7,9 @@ describe('LeetcodeService', () => {
   let service: LeetcodeService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(LeetcodeService);
   });
 
