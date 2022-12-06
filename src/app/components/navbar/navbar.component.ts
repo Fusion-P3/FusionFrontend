e69a4550d1c896c6c714e519abde204c6bbbcc6e
James Monty-Carbonari <james@machinateddesigns.com>
2022-12-06 18:08:10 -0500

    styles complete?

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { InventoryDTO } from 'src/app/models/inventoryDTO';
+import { InventoryService } from 'src/app/services/inventory.service';
 import { AuthService } from 'src/app/services/auth.service';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -11,16 +13,30 @@ import { ProductService } from 'src/app/services/product.service';
 })
 export class NavbarComponent implements OnInit {
 
+  inventory: InventoryDTO[] = [];
   cartCount!: number;
   totalPrice!: number;
   subscription!: Subscription;
+  totalHydrogen!: number;
 
-  constructor(private authService: AuthService, private router: Router, private productService: ProductService) { }
+
+  constructor(private authService: AuthService, private router: Router, private productService: ProductService, private inventoryService: InventoryService) { }
 
   ngOnInit(): void {
+
+    this.inventoryService.getInventory(this.authService.userId).subscribe((res) => {
+res.forEach(x => { 
+  if(x.productName == 'Hydrogen') {
+    this.totalHydrogen = x.quantity
+  }
+
+})
+    })
+
     this.subscription = this.productService.getCart(this.authService.userId).subscribe(
       (cart) => this.cartCount = cart.cartCount
     );
+
     this.subscription = this.productService.getCart(this.authService.userId).subscribe(
       (cart) => this.totalPrice = cart.products.reduce(
         (a, b) => a + b.product.price * b.quantity,
