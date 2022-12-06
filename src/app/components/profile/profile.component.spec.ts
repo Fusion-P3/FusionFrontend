1feef339d959e5651bc24cff7c56579f64f0bbba
Gregory Sabado <gregory793@revature.net>
2022-12-06 12:13:48 -0800

    fixed profile test

--- src/app/components/profile/profile.component.spec.ts
+++ src/app/components/profile/profile.component.spec.ts
@@ -1,5 +1,7 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 import { ProfileComponent } from './profile.component';
 
@@ -9,8 +11,8 @@ describe('ProfileComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-      declarations: [ProfileComponent],
+      imports: [HttpClientTestingModule, RouterTestingModule],
+      declarations: [ProfileComponent, NavbarComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProfileComponent);
